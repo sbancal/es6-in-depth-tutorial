8a30ee367530e1606c398827fb487f235d447b40
Samuel Bancal <Samuel.Bancal@epfl.ch>
2017-08-21 11:58:26 +0200

    +arrow functions +modules

---
+++ SB/es6/app/fellowship.js
@@ -0,0 +1,3 @@
+const fellowship = ['Alice', 'Bob', 'Charles']
+const total = fellowship.length
+export { fellowship, total }
--- SB/es6/app/index.js
+++ SB/es6/app/index.js
@@ -23,3 +23,64 @@ console.log(`name is ${name} and age is ${age}.`);
  a block. So we need to encapsulate it into (). */
 ({name, age} = king)
 console.log(`name is ${name} and age is ${age}.`);
+
+
+// Arrow functions
+// function() {
+//   // This is pre-es6 syntax
+// }
+
+() => {
+  // This is es6 syntax
+  // function is _anonymous_
+}
+
+// With map helper methods:
+let values = [20, 30, 40]
+let doubled = values.map((n) => {
+  return n*2
+})
+let doubledWithLessCode = values.map((n) => n*2)
+
+// With filter helper methods:
+let points = [15, 3, 18, 23, 10, 32]
+let highScores = points.filter((n) => {
+  return n > 15
+})
+let highScoresWithLessCode = points.filter((n) => n > 15)
+
+
+// String helpers
+console.log('String helpers')
+let msg = 'wooh' + 'oo'.repeat(50)
+console.log(msg)
+// woohoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+
+msg = `wooh${'oo'.repeat(50)}`
+console.log(msg)
+// woohoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+
+console.log('butterfly'.startsWith('butter')) // true
+console.log('butterfly'.includes('tt')) // true
+console.log('butterfly'.endsWith('fly')) // true
+
+
+// Number helpers
+console.log('Number helpers')
+console.log(Number.isFinite(32)) // true
+console.log(Number.isFinite(Infinity)) // false
+console.log(Number.isSafeInteger(321)) // true
+console.log(Number.isSafeInteger(Math.pow(2, 54))) // false
+
+
+// Modules
+console.log('Modules')
+import { fellowship, total } from './fellowship'
+console.log(fellowship, total) // (3) ["Alice", "Bob", "Charles"] 3
+
+import { add, multiply } from './math'
+console.log(add(2, 3)) // 5
+console.log(multiply(2, 3)) // 6
+
+import defMultiply from './math'
+console.log(defMultiply(3, 4)) // 12
