96813179fb08b30567df6876393de54d6d3ae803
Samuel Bancal <Samuel.Bancal@epfl.ch>
2017-12-11 15:48:26 +0100

    +Gallery details

--- SB/react-demo/app/components/Gallery.js
+++ SB/react-demo/app/components/Gallery.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react'
 
 class Gallery extends Component {
   render() {
+    let alternateImage = 'https://cdn1.iconfinder.com/data/icons/ios-edge-line-7/25/No-Camera-128.png'
+
     return (
       <div>
         {
           this.props.items.map((item, index) => {
-            let { title } = item.volumeInfo;
+            let { title, imageLinks, infoLink } = item.volumeInfo;
             return (
-              <div key={index}>{title}</div>
+              <a
+                key={index}
+                className="book"
+                href={infoLink}
+                target="_blank"
+              >
+                <img
+                  src={imageLinks !== undefined ? imageLinks.thumbnail : alternateImage}
+                  alt="book image"
+                  className="book-img"
+                />
+                <div className="book-text">
+                {title}
+                </div>
+              </a>
             )
           })
         }
