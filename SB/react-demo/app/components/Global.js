8a76492bdebee78829392f3f34f74f7a1e1a4413
Samuel Bancal <Samuel.Bancal@epfl.ch>
2017-12-11 14:53:02 +0100

    +react state

--- SB/react-demo/app/components/Global.js
+++ SB/react-demo/app/components/Global.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react'
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap'
 
 class Global extends Component {
+  constructor (props){
+    super(props)
+    this.state = {
+      query: ''
+    }
+  }
+
   search() {
-    console.log('search')
+    console.log('search', this.state.query)
   }
 
   render() {
@@ -15,6 +22,12 @@ class Global extends Component {
             <FormControl
               type="text"
               placeholder="Search for a book"
+              onChange={event => this.setState({query: event.target.value})}
+              onKeyPress={event => {
+                if (event.key === 'Enter'){
+                  this.search()
+                }
+              }}
             />
             <InputGroup.Addon onClick={() => this.search()}>
               <Glyphicon glyph="search"></Glyphicon>
