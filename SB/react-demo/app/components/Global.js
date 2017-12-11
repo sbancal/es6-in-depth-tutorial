6603045620e336b61a0c6b9c3816476ec2f202a2
Samuel Bancal <Samuel.Bancal@epfl.ch>
2017-12-11 15:22:13 +0100

    +Gallery component

--- SB/react-demo/app/components/Global.js
+++ SB/react-demo/app/components/Global.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap'
+import Gallery from './Gallery'
+
 
 class Global extends Component {
   constructor (props){
     super(props)
     this.state = {
-      query: ''
+      query: '',
+      items: []
     }
   }
 
@@ -13,7 +16,10 @@ class Global extends Component {
     const BASE_URL = 'https://www.googleapis.com/books/v1/volumes?q='
     fetch(`${BASE_URL}${this.state.query}`, { method: 'GET' })
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then(json => {
+      let { items } = json
+      this.setState({items})
+    })
   }
 
   render() {
@@ -37,6 +43,7 @@ class Global extends Component {
             </InputGroup.Addon>
           </InputGroup>
         </FormGroup>
+        <Gallery items={this.state.items}/>
       </div>
     )
   }
