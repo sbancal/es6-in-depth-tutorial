34fc7ca7c1ff88b4e41d89d50cc534f3278948de
Samuel Bancal <Samuel.Bancal@epfl.ch>
2017-12-11 14:43:59 +0100

    +react search form

--- SB/react-demo/app/components/Global.js
+++ SB/react-demo/app/components/Global.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react'
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap'
 
 class Global extends Component {
+  search() {
+    console.log('search')
+  }
+
   render() {
     return (
-      <div>The Global App Component</div>
+      <div>
+        <h2>Book Explorer!</h2>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Search for a book"
+            />
+            <InputGroup.Addon onClick={() => this.search()}>
+              <Glyphicon glyph="search"></Glyphicon>
+            </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
+      </div>
     )
   }
 }
